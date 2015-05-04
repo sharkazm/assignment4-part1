45179db90a9c542bdebc6cc23c5623aedd059c29
sharkazm <sharkazm@onid.oregonstate.edu>
2015-05-04 11:36:30 -0400

    Update login.php

--- src/login.php
+++ src/login.php
@@ -1,1 +1,21 @@
+//Resources:  All code was developed using the lectures and resources from OSU's CS 290 course and the PHP manual.  
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Login</title>
+	</head>
+
+<body>
+
+	<h3>Enter your username:  </h3>
+
+	<form action="content1.php" method="POST">
+		Username: <input type="text" name="username">
+		<input type="submit" value="Login">
+	</form>
+
+</body>
+</html>
 
